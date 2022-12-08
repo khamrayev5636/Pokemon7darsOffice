82a220db886e59d70d84921a2dbd0bf1fff35178
khamrayev5636 <khamrayev5636@gmail.com>
2022-12-08 12:58:28 +0500

    Add js file

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
+ AOS.init();
+
 const elList = document.querySelector(".pokemon__list");
 
-const pokeSlice = pokemons.slice(0 , 10)
+// const pokeSlice = pokemons.slice(0 , 10)
 
-for (const pokemon of pokeSlice) {
+for (const pokemon of pokemons) {
     
     // CreateElement yaratish 
 
     const newItem = document.createElement("li");
-    newItem.classList.add("item-js")
+    newItem.classList.add("item-js");
+    newItem.setAttribute("data-aos" , "fade-up");
+    newItem.setAttribute("data-aos-duration" , "2000")
     const newNum = document.createElement("span");
     newNum.classList.add("num-js")
     const newImg = document.createElement("img");
@@ -17,9 +21,9 @@ for (const pokemon of pokeSlice) {
     const newDivBadge = document.createElement("div");
     newDivBadge.classList.add("badge-js");
     const newBadge = document.createElement("span");
-    newBadge.classList.add("badge" , "text-bg-warning" , "mb-2")
+    newBadge.classList.add("badge" , "text-bg-warning" , "mb-3")
     const newTitle = document.createElement("h3");
-    newTitle.classList.add("text-white" , "text-center" , "fs-3" , "mb-4")
+    newTitle.classList.add("text-white" , "text-center" , "fs-4" , "mb-4")
     const newText = document.createElement("p");
     newText.classList.add("text-js")
 
